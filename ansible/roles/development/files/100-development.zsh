e21398cbf8a8f7bd7475b4315fb3712c9e9ccb90
Caleb Stewart <caleb.stewart94@gmail.com>
2023-04-03 01:27:10 -0400

    Added development role

---
+++ ansible/roles/development/files/100-development.zsh
@@ -0,0 +1,5 @@
+# Nicer place to house golang files
+export GOPATH="$HOME/.local/share/go"
+
+# Add golang installed binaries to the path
+export PATH="$PATH:$HOME/.local/share/go/bin:$HOME/.config/emacs/bin"
