c4f9f7bce406553066ca43cd49d932cdfd4ce519
Caleb Stewart <caleb.stewart94@gmail.com>
2023-04-09 05:08:47 -0400

    Removed extra echo :facepalm:

--- ansible/roles/ryzen/files/looking-glass/wrapper.sh
+++ ansible/roles/ryzen/files/looking-glass/wrapper.sh
@@ -3,7 +3,6 @@
 output=$(looking-glass-client 2>&1)
 
 if [ "$?" -ne 0 ]; then
-    echo "$output"
     error_info=$(echo "$output" | grep "Error: " | head -n1 | cut -d':' -f2 | awk '{$1=$1};1')
     notify-send "Looking Glass" "$error_info" \
         --icon "$HOME/.local/share/icons/looking-glass.png" \
