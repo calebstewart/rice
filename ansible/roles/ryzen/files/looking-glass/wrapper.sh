01214c5c45b3731bde51ed62a3fcb4238505143f
Caleb Stewart <caleb.stewart94@gmail.com>
2023-04-09 05:07:22 -0400

    Improved Error Notification for Looking Glass Wrapper

--- ansible/roles/ryzen/files/looking-glass/wrapper.sh
+++ ansible/roles/ryzen/files/looking-glass/wrapper.sh
@@ -3,9 +3,9 @@
 output=$(looking-glass-client 2>&1)
 
 if [ "$?" -ne 0 ]; then
-    error_info=$(echo "$output" | grep "Error: " | head -n1 | cut -d':' -f1 | awk '{$1=$1};1')
+    echo "$output"
+    error_info=$(echo "$output" | grep "Error: " | head -n1 | cut -d':' -f2 | awk '{$1=$1};1')
     notify-send "Looking Glass" "$error_info" \
-        --icon "looking-glass" \
-        --app-name "looking-glass" \
-        --urgency "critical"
+        --icon "$HOME/.local/share/icons/looking-glass.png" \
+        --app-name "looking-glass"
 fi
